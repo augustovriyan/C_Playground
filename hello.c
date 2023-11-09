d2e08227aa87a3da1c3e2fd29b3999bf2e7d81af
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-09 18:58:01 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+#define greet printf
+#define planet "Earth"
+#define punctuation "!"
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+   // greet() displays the string inside quotation
+   greet("Greetings, %s%s", planet, punctuation);
    return 0;
 }
