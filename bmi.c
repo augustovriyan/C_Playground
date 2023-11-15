233ee218bdf2fc45ca58c6fa6568ee7d53515887
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-15 19:42:57 +0700

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -3,13 +3,15 @@
 int main() {
     float weight, height, bmi;
 
-    // Input weight in kilograms
-    printf("Enter your weight in kilograms: ");
-    scanf("%f", &weight);
+    // Input weight in kilograms with validation
+    do {
+        printf("Enter your weight in kilograms: ");
+    } while (scanf("%f", &weight) != 1 || weight <= 0);
 
-    // Input height in meters
-    printf("Enter your height in meters: ");
-    scanf("%f", &height);
+    // Input height in meters with validation
+    do {
+        printf("Enter your height in meters: ");
+    } while (scanf("%f", &height) != 1 || height <= 0);
 
     // Calculate BMI
     bmi = weight / (height * height);
