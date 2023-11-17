cf8003d5ec1bec6a87a30d993247f7e805eef0e4
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-17 20:15:00 +0700

    Update timer.c

--- timer.c
+++ timer.c
@@ -7,7 +7,7 @@ void start_countdown(int seconds) {
     while (seconds > 0) {
         int mins = seconds / 60;
         int secs = seconds % 60;
-        printf("\r%02d:%02d", mins, secs);
+        printf("\rTime Remaining: %02d:%02d", mins, secs);
         fflush(stdout);
         sleep(1);
         seconds--;
@@ -18,11 +18,13 @@ void start_countdown(int seconds) {
 int main() {
     int timer_duration;
 
-    printf("Enter the timer duration in seconds: ");
-    if (scanf("%d", &timer_duration) != 1 || timer_duration <= 0) {
-        printf("Invalid timer duration. Please enter a positive integer.\n");
-        return 1; // Return an error code
-    }
+    // Get user input for timer duration with validation
+    do {
+        printf("Enter the timer duration in seconds (positive integer): ");
+    } while (scanf("%d", &timer_duration) != 1 || timer_duration <= 0);
+
+    // Provide initial information
+    printf("Starting countdown timer for %d seconds...\n", timer_duration);
 
     // Start countdown timer based on user input
     start_countdown(timer_duration);
