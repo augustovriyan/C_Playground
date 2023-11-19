36aa206895928579912ea0531dfa1f505b83ab2e
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-19 18:07:23 +0700

    Update alarm.c

--- alarm.c
+++ alarm.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>  // Include the necessary header for usleep
+#include <unistd.h>
+
+void setAlarm(int seconds);
 
 int main() {
     int seconds;
@@ -13,17 +15,22 @@ int main() {
     // Validate user input
     if (seconds <= 0) {
         printf("Invalid input. Please enter a positive number of seconds.\n");
-        return EXIT_FAILURE;  // Exit with an error code using EXIT_FAILURE constant
+        return EXIT_FAILURE;
     }
 
     // Inform the user about the alarm setting
     printf("Setting the alarm for %d seconds...\n", seconds);
 
-    // Sleep for the specified number of seconds using usleep for finer granularity
-    usleep(seconds * 1000000);
+    // Call the function to set the alarm
+    setAlarm(seconds);
 
     // Display the alarm message
     printf("Time's up! Alarm triggered after %d seconds.\n", seconds);
 
-    return EXIT_SUCCESS;  // Exit with success using EXIT_SUCCESS constant
+    return EXIT_SUCCESS;
+}
+
+void setAlarm(int seconds) {
+    // Sleep for the specified number of seconds using usleep for finer granularity
+    usleep(seconds * 1000000);
 }
