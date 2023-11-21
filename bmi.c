90d411de4809d761a67dde0903003df18155426e
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-21 19:03:02 +0700

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define BMI_PRECISION 2
+
+// Function to get valid input for weight
+float getValidWeight() {
+    float weight;
+    do {
+        printf("Enter your weight in kilograms: ");
+        if (scanf("%f", &weight) != 1 || weight <= 0) {
+            printf("Invalid input. Please enter a valid weight greater than 0.\n");
+            // Clear the input buffer
+            while (getchar() != '\n');
+        }
+    } while (weight <= 0);
+    return weight;
+}
+
+// Function to get valid input for height
+float getValidHeight() {
+    float height;
+    do {
+        printf("Enter your height in meters: ");
+        if (scanf("%f", &height) != 1 || height <= 0) {
+            printf("Invalid input. Please enter a valid height greater than 0.\n");
+            // Clear the input buffer
+            while (getchar() != '\n');
+        }
+    } while (height <= 0);
+    return height;
+}
 
 int main() {
     float weight, height, bmi;
 
-    // Input weight in kilograms with validation
-    do {
-        printf("Enter your weight in kilograms: ");
-    } while (scanf("%f", &weight) != 1 || weight <= 0);
-
-    // Input height in meters with validation
-    do {
-        printf("Enter your height in meters: ");
-    } while (scanf("%f", &height) != 1 || height <= 0);
+    // Input weight and height with validation
+    weight = getValidWeight();
+    height = getValidHeight();
 
     // Calculate BMI
     bmi = weight / (height * height);
 
     // Display the calculated BMI
-    printf("Your BMI is: %.2f\n", bmi);
+    printf("Your BMI is: %.*f\n", BMI_PRECISION, bmi);
 
     return 0;
 }
