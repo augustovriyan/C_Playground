5c8fc3ea364f0b8d5a1b06951bbf7bc9bbe7733b
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-23 22:09:55 +0700

    Update donut.c

--- donut.c
+++ donut.c
@@ -26,13 +26,19 @@ int main() {
             for (int j = 0; j < WIDTH; j++) {
                 float x = j - WIDTH / 2.0;
                 float y = i - HEIGHT / 2.0;
+
+                // Parametric equations for the torus
                 float d1 = 0.5 * sin(A);
                 float d2 = 0.5 * cos(B);
                 float z = x * x + y * y;
+
+                // Calculate intensity based on distance to the torus surface
                 int intensity = (int)(8 * ((d1 / sqrt(z + d2 * d2)) + 1));
+
+                // Map intensity to characters
                 output[i][j] = CHARACTERS[intensity];
             }
-            output[i][WIDTH] = '\0';
+            output[i][WIDTH] = '\0'; // Null-terminate the string
         }
 
         printf("\033[H"); // Move cursor to home position
@@ -40,6 +46,7 @@ int main() {
             printf("%s\n", output[i]);
         }
 
+        // Update parameters for animation
         A += A_INCREMENT;
         B += B_INCREMENT;
 
