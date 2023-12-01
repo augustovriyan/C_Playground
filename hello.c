73e179b934e4049773b7bb22a24502b602b5c41c
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-30 16:54:34 -0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+// Let's add some fun modifications
 #define greet printf
 #define planet "Earth"
 #define punctuation "!"
 
+// Function to add a little fun twist
+void funMessage() {
+    printf("\nBut beware, intergalactic squirrels might be watching!\n");
+}
+
 int main() {
-   // greet() displays the string inside quotation
-   greet("Greetings, %s%s", planet, punctuation);
-   return 0;
+    // greet() displays the string inside quotation
+    greet("Greetings, %s%s", planet, punctuation);
+    
+    // Call the funMessage function
+    funMessage();
+
+    return 0;
 }
