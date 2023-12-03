44128017760040c5dac8702eca4e74b64ab75731
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-12-03 11:16:36 -0800

    Update timer.c

--- timer.c
+++ timer.c
@@ -15,13 +15,23 @@ void startCountdown(int seconds) {
     printf("\rTime's up!\n");
 }
 
-int main() {
-    int timerDuration;
-
-    // Get user input for timer duration with validation
-    do {
+int getPositiveInteger() {
+    int value;
+    while (1) {
         printf("Enter the timer duration in seconds (positive integer): ");
-    } while (scanf("%d", &timerDuration) != 1 || timerDuration <= 0);
+        if (scanf("%d", &value) == 1 && value > 0) {
+            return value;
+        } else {
+            // Clear the input buffer
+            while (getchar() != '\n');
+            printf("Invalid input. Please enter a positive integer.\n");
+        }
+    }
+}
+
+int main() {
+    // Get user input for timer duration with validation
+    int timerDuration = getPositiveInteger();
 
     // Provide initial information
     printf("\nStarting countdown timer for %d seconds...\n", timerDuration);
